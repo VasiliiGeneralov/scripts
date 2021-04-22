61472ffafb16c2b559ee31a3ec3fea8b39df1230
Vasilii Generalov <vgeneralov@kraftway.ru>
2021-04-22 09:52:55 +0300

    feat: add alacritty build script

---
+++ alacritty/build_and_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+git fetch $(git remote show | head -n 1)
+git pull --rebase --recurse-submodules
+rustup override set stable
+rustup update stable
+unset CC && export CC=$(which clang-12) && echo $CC
+unset CXX && export CXX=$(which clang++-12) && echo $CXX
+sudo apt install\
+  cmake\
+  pkg-config\
+  libfreetype6-dev\
+  libfontconfig1-dev\
+  libxcb-xfixes0-dev\
+  python3
+cargo build --release
+infocmp alacritty
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+sudo cp target/release/alacritty /usr/local/bin
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
