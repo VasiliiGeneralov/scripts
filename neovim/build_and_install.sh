7a57ea261dfc3a525fc73eff23a56e998db89cf4
Vasilii Generalov <vgeneralov@kraftway.ru>
2021-04-22 09:53:17 +0300

    feat: add neovim build script

---
+++ neovim/build_and_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git fetch $(git remote show | head -n 1)
+git pull --rebase --recurse-submodules
+unset CC && export CC=$(which clang-12) && echo $CC
+unset CXX && export CXX=$(which clang++-12) && echo $CXX
+sudo rm -rf build .deps
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
