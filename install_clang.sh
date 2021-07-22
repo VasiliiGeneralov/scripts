a2874bfeb1026e1c4fac301c09fc476e850365f7
Vasilii Generalov <v.generalov@parthner.samsung.com>
2021-07-22 14:49:11 +0300

    feat: add clang and co installation script

---
+++ install_clang.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+apt install\
+ libllvm-12-ocaml-dev\
+ libllvm12\
+ llvm-12\
+ llvm-12-dev\
+ llvm-12-doc\
+ llvm-12-examples\
+ llvm-12-runtime\
+ clang-12\
+ clang-tools-12\
+ clang-12-doc\
+ libclang-common-12-dev\
+ libclang-12-dev\
+ libclang1-12\
+ clang-format-12\
+ clang-tidy-12\
+ python3-clang-12\
+ clangd-12\
+ libfuzzer-12-dev\
+ lldb-12\
+ lld-12\
+ libc++-12-dev libc++abi-12-dev\
+ libomp-12-dev\
+ libclc-12-dev
