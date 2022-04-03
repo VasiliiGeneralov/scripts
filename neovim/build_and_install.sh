a07cbbf0af66836e158182dffb4dca554c486fdc
Vasilii Generalov <begulme@gmail.com>
2022-04-03 16:20:17 +0300

    Update neovim build script

--- neovim/build_and_install.sh
+++ neovim/build_and_install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-git fetch $(git remote show | head -n 1)
 git pull --rebase --recurse-submodules
-unset CC && export CC=$(which clang-12) && echo $CC
-unset CXX && export CXX=$(which clang++-12) && echo $CXX
-sudo rm -rf build .deps
-make CMAKE_BUILD_TYPE=RelWithDebInfo
+make CMAKE_BUILD_TYPE=Release
 sudo make install
-git log --oneline -1; nvim --version | head -n 1
