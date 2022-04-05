e81d2d6c73a9b692f53327e8a6e315d638e6ecbd
Vasilii Generalov <begulme@gmail.com>
2022-04-06 00:40:02 +0300

    Update tmux build script

--- tmux/build_and_install.sh
+++ tmux/build_and_install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-git fetch $(git remote show | head -n 1)
 git pull --rebase --recurse-submodules
-unset CC && export CC=$(which clang-12) && echo $CC
-unset CXX && export CXX=$(which clang++-12) && echo $CXX
-./autogen.sh
-./configure && make
-sudo make install
+sudo apt install\
+  libevent-dev\
+  ncurses-dev\
+  build-essential\
+  bison\
+  pkg-config\
+  autoconf\
+  automake
+sh autogen.sh
+./configure
+make && sudo make install
