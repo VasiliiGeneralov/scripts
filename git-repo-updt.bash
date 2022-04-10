709c3225c57614e4b9c59b27e07b60d5f74c8246
Vasilii Generalov <begulme@gmail.com>
2022-04-10 22:42:17 +0300

    List available tags
    Signed-off-by: Vasilii Generalov <begulme@gmail.com>

--- git-repo-updt.bash
+++ git-repo-updt.bash
@@ -15,6 +15,21 @@ do
     git fetch &> /dev/null
     echo "${DIR##*/}:"
     git status | head -n 2
+    tags=$(git tag -l '[0-9]*.[0-9]*' 'v[0-9]*.[0-9]*')
+    if [[ -n "${tags}" ]]; then
+      printf "Available tags:\n"
+      i=0
+      for tag in ${tags}
+      do
+        printf "%+12s " "${tag}"
+        (( ++i ))
+        if [[ i -ge 8 ]]; then
+          i=0
+          printf "\n"
+        fi
+      done
+      printf "\n"
+    fi
     echo "-------------------------"
     popd &> /dev/null || exit 2
   fi
