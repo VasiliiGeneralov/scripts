01d381e146f37eda3fccea74ddbfdedc1e7d715f
Vasilii Generalov <begulme@gmail.com>
2022-04-17 13:58:12 +0300

    Build and install not as root

--- neovim/build_and_install.sh
+++ neovim/build_and_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git pull --rebase --recurse-submodules
-sudo make CMAKE_BUILD_TYPE=Release && sudo make install
+make CMAKE_BUILD_TYPE=Release && make CMAKE_INSTALL_PREFIX="${HOME}/.local" install
