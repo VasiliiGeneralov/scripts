a70cbe072c05255620763b70764e1cb81d8d9708
Vasilii Generalov <vasilii.generalov@gmail.com>
2022-07-12 20:43:55 +0300

    Update script
    Signed-off-by: Vasilii Generalov <vasilii.generalov@gmail.com>

--- lscpu2int.py
+++ lscpu2int.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 def lscpu_to_int(lscpu_entry: str) -> int:
+
+    def __apply_lscpu_suffix(digit, suffix: str):
+        result = digit
+        if suffix in ['KB', 'KiB']:
+            result = result * 1024
+        elif suffix in ['MB', 'MiB']:
+            result = result * 1024 * 1024
+        return result
+
+    if not lscpu_entry:
+        return -1
     suffix_pos = 0
     for char in lscpu_entry:
         if not char.isalpha():
@@ -9,16 +20,17 @@ def lscpu_to_int(lscpu_entry: str) -> int:
             break
     lscpu_amount_str = lscpu_entry[:suffix_pos].lstrip().rstrip()
     lscpu_suffix = lscpu_entry[suffix_pos:].lstrip().rstrip()
-    lscpu_amount_digit = None
-    if lscpu_amount_str.isdigit():
+    if not lscpu_amount_str:
+        return -1
+    try:
         lscpu_amount_digit = 0
         for digit in lscpu_amount_str:
             lscpu_amount_digit = (lscpu_amount_digit * 10) + int(digit)
-    else:
+        return __apply_lscpu_suffix(lscpu_amount_digit, lscpu_suffix)
+    except ValueError:
+        pass
+    try:
         lscpu_amount_digit = float(lscpu_amount_str)
-    result = lscpu_amount_digit
-    if lscpu_suffix in ['KB', 'KiB']:
-        result = result * 1024
-    elif lscpu_suffix in ['MB', 'MiB']:
-        result = result * 1024 * 1024
-    return result
+        return __apply_lscpu_suffix(lscpu_amount_digit, lscpu_suffix)
+    except ValueError:
+        return -1
